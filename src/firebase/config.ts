c216bfe10e88abba768db969bf12afa895a5e9c5
Pawarat Banlengsong <Pawabanleng@gmail.com>
2026-02-24 03:34:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8476966418-d9e4a",
+  "appId": "1:994444285223:web:dd085beae95e5160688502",
+  "apiKey": "AIzaSyCeWbh0arVa5pDTqmLSskXwqRbi4lTWcSg",
+  "authDomain": "studio-8476966418-d9e4a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "994444285223"
+};
